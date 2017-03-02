b39b923b1f945e69fc3dced01ec62ba02b4e4ada
qkfreas <qkfreas@gmail.com>
2017-03-02 10:39:40 -0500

    began password dev

--- lib/auth.php
+++ lib/auth.php
@@ -1,7 +1,7 @@
 <?php
 if(!empty($_POST['password'])) {
     if(md5('blah@#$'.sha1('3NhNj8&'.$_POST['password']) ) =='MD5 value of your password' ) {
-        header("Location: https://www.google.com/"); /* Redirect here if the password is correct */
+        header("Location: deals.html"); /* Redirect here if the password is correct */
     }
     else {
         header("Location: https://www.google.com/"); /* Return here if the password isn't correct */
