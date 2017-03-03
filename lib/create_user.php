06c38247a4094999240bb76f52a94c892deaaeab
qkfreas <qkfreas@gmail.com>
2017-03-03 17:20:25 -0500

    Completed PHP to SQL communication

--- lib/create_user.php
+++ lib/create_user.php
@@ -1,13 +1,31 @@
 <?php
-$servername="localhost";
+$servername="DESKTOP-PAU76IG";
 $username = "qkfreas";
 $password = "password";
 $dbname = "user_info";
 
-mssql_connect($servername,$username,$password,$dbname);
-if (!empty($_POST['new_password']) && !empty($_POST['new_username'])) {
-    $temp_password = md5('blah@#$'.sha1('3NhNj8&'.$_POST['password']));
-    $temp_username = $_POST['new_username'];
-    mssql_query("INSERT into users(user_name,user_pass) VALUES ($temp_username,$temp_password)" );
+$connectionInfo = array("Database"=>$dbname,"UID" =>$username, "PWD"=>$password, "MultipleActiveResultSets"=>true);
+
+$conn = sqlsrv_connect($servername,$connectionInfo);
+if($conn === false) {
+    die(print_r(sqlsrv_errors(), true));
 }
+
+
+if ($_REQUEST["new_password"] && $_REQUEST["new_username"]) {
+    $temp_password = md5('blah@#$'.sha1('3NhNj8&'.$_REQUEST["new_password"]));
+    $temp_username = $_REQUEST["new_username"];
+
+    $sql = "INSERT INTO users(user_name, user_pass) VALUES(?, ?)";
+    $params = array($temp_username, $temp_password);
+
+    $stmt = sqlsrv_query($conn, $sql, $params);
+    if($stmt === false) {
+        die(print_r(sqlsrv_errors(), true));
+    }
+    else {
+        echo "Record add successful";
+    }
+}
+sqlsrv_close($conn);
 ?>
