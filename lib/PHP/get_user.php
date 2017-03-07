82eed39b7a508566ed2e981c25d7cdfde2782620
qkfreas <qkfreas@gmail.com>
2017-03-07 13:12:32 -0500

    Renamed gtdata.php

--- lib/PHP/get_user.php
+++ lib/PHP/get_user.php
@@ -6,7 +6,7 @@
  * Time: 9:58 AM
  */
 header('Location: index.php');
-    $servername="DESKTOP-PAU76IG";
+//    $servername="DESKTOP-PAU76IG";
     $servername="LENOVO-QF";
     $username = "qkfreas";
     $password = "password";
@@ -25,23 +25,23 @@ if ($_REQUEST["password"] && $_REQUEST["username"]) {
     $temp_username = $_REQUEST["username"];
 
 
-    $sql = 'SELECT user_name,user_pass FROM users';
+    /*$sql = 'SELECT user_name,user_pass FROM users';
     $retval = sqlsrv_query($conn, $sql);
 
     if (!$retval) {
         die(print_r(sqlsrv_errors(), true));
     }
 
-    $fetched_user_name = NULL;
-    $fetched_user_pass = NULL;
-    while ($row = sqlsrv_fetch_array($retval, SQLSRV_FETCH_ASSOC)) {
-        $fetched_user_name = $row['user_name'];
-        $fetched_user_pass = $row['user_pass'];
-    }
+    $fetched_user_pass = NULL;*/
 
-    if ($fetched_user_name === $temp_username && $fetched_user_pass === $temp_password) {
-        echo "username is correct\n";
-        $_SESSION['id'] = $fetched_user_name;
+    $getit = sqlsrv_query("SELECT COUNT(*) FROM users WHERE user_name='$temp_username' AND user_pass='$temp_password'", $conn);
+    $row = sqlsrv_fetch_array($getit);
+    if (!$row)
+        die(print_r(sqlsrv_errors(), true));
+
+
+    if (count($row) == 100) {
+        $_SESSION['id'] = true;
         session_start('id');
         header('Location: index.php');
     }
--- lib/PHP/login.php
+++ lib/PHP/login.php
@@ -6,7 +6,6 @@
  * Date: 3/6/2017
  * Time: 2:45 PM
  */
-//header('Location: index.php');
 
 include ("../HTML/header.html");
 include("../HTML/login.html");
