44fd7b839f0dfdacf46c0721edc31c1dc93de103
qkfreas <qkfreas@gmail.com>
2017-03-17 12:39:13 -0400

    Edited checkout page. Create confirmation page.

---
+++ lib/PHP/confirm.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: Quinn F
+ * Date: 3/17/2017
+ * Time: 12:31 PM
+ */
+include ("../HTML/header.html");
+include ("../HTML/confirmation.html");
--- lib/PHP/create_your_own.php
+++ lib/PHP/create_your_own.php
@@ -5,16 +5,30 @@
  * Date: 3/13/2017
  * Time: 10:44 AM
  */
+session_start();
 header("Location:../PHP/checkout.php");
 $crust_selected = $_GET['crust_type'];
 $toppings_selected = $_GET['toppings'];
 
-$pizza = array($crust_selected, $toppings_selected);
+if(!$_SESSION['number_of_pizzas'] >= 1) {
+    $_SESSION['user_selection'] = "";
+    $_SESSION['number_of_pizzas'] = 1;
+    $_SESSION['number_of_toppings'] = 0;
+    $_SESSION['grand_total'] = 0.00;
+}
+else {
+    $_SESSION['number_of_pizzas'] = $_SESSION['number_of_pizzas'] + 1;
+}
 
-$temp = $_SESSION['user_selection'];
-reset($_SESSION['user_selection']);
-array_push($temp, $pizza);
-
-$_SESSION['user_selection'] = $temp;
+$temp = $_SESSION['user_selection']."Pizza ".$_SESSION['number_of_pizzas'].":\n";
+$temp = $temp.$crust_selected;
+foreach($toppings_selected as $value) {
+    $temp = $temp."\n".$value;
+}
+$temp = $temp."\n\n";
+$_SESSION["user_selection"] = $temp;
+$_SESSION['number_of_toppings'] = $_SESSION['number_of_toppings'] + sizeof($toppings_selected);
+$_SESSION['grand_total'] = $_SESSION['grand_total']+ 12.95+(sizeof($toppings_selected)*.5);
 unset($temp);
+
 ?>
