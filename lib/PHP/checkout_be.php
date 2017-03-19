3fdbc4a092effbea905f6ccc7fb8de3724174b4b
qkfreas <qkfreas@gmail.com>
2017-03-19 12:40:46 -0400

    Added order type and updated SQL input

--- lib/PHP/checkout_be.php
+++ lib/PHP/checkout_be.php
@@ -13,6 +13,7 @@ if(isset($_SESSION['id']) && $_SESSION['id'] === true) {
 
     $street1Err = $cityErr = $stateErr = $zip_codeErr = $phone_numberErr = "";
     $street1 = $street2 = $city = $state = $zip_code = $phone_number = "";
+    $delivery_type = $_REQUEST['order_type'];
 
     $credit_number = $cvv_code = $exp_date = "";
 
@@ -48,7 +49,7 @@ if(isset($_SESSION['id']) && $_SESSION['id'] === true) {
         $phone_number = $_REQUEST["phone_number"];
     }
 
-    $credit_number = md5('blah@#$' . sha1('3NhNj8&' . $_REQUEST["credit_number"]));
+    $credit_number = md5('blah@#$' . sha1('3NhNj8&'.$_REQUEST["credit_number"]));
     $exp_date = $_REQUEST["exp_date"];
     $cvv_code = $_REQUEST["cvv_code"];
 
@@ -57,10 +58,14 @@ if(isset($_SESSION['id']) && $_SESSION['id'] === true) {
         die(print_r(sqlsrv_errors(), true));
     }
 
-    $sql = "UPDATE users SET street_1='".$street1."', street_2='".$street2."', city='".$city."', state_re='".$state."', zip_code='".$zip_code."', phone_number='".$phone_number."', credit_number ='".$credit_number."', exp_date='".$exp_date."', cvv_code='".$cvv_code."'  WHERE user_name='".$_SESSION['username']."'";
-    $params = array($street1, $street2, $city, $state, $zip_code, $phone_number);
+    $temp_name = $_SESSION['username'];
+    $temp_selection = $_SESSION['user_selection'];
+    $temp_total = $_SESSION['grand_total'];
+    $sql = "UPDATE users SET street_1='".$street1."', street_2='".$street2."', city='".$city."', state_re='".$state."', zip_code='".$zip_code."', phone_number='".$phone_number."', credit_number ='".$credit_number."', exp_date='".$exp_date."', cvv_code='".$cvv_code."'  WHERE user_name='".$_SESSION['username']."';
+    INSERT INTO orders (user_name,order_summary,total,order_type) VALUES ('$temp_name','$temp_selection','$temp_total','$delivery_type');
+    ";
 
-    $stmt = sqlsrv_query($conn, $sql/*, $params*/);
+    $stmt = sqlsrv_query($conn, $sql);
     if ($stmt === false) {
         die(print_r(sqlsrv_errors(), true));
     } else {
--- lib/PHP/create_user.php
+++ lib/PHP/create_user.php
@@ -11,8 +11,8 @@ if ($_REQUEST["new_password"] && $_REQUEST["new_username"]) {
     $temp_password = md5('blah@#$'.sha1('3NhNj8&'.$_REQUEST["new_password"]));
     $temp_username = $_REQUEST["new_username"];
 
-    $sql = "INSERT INTO users(user_name, user_pass) VALUES(?, ?)";
-    $params = array($temp_username, $temp_password);
+    $sql = "INSERT INTO users(user_name, user_pass, user_level) VALUES(?,?,?)";
+    $params = array($temp_username, $temp_password, 0);
 
     $stmt = sqlsrv_query($conn, $sql, $params);
     if($stmt === false) {
